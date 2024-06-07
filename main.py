9bb4b9e3e61baf99a997be4cfa1733b56d74890d
estelah <emfj.estela@gmail.com>
2024-06-06 21:10:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,67 @@
 import os
 
+#array restaurantes
 restaurantes = ['Pizza', 'Sushi']
 
+#nome do app
 def exibir_nome_programa():
         print('''
               Happy Veggi''')
 
+#opções de ações no app
 def exibir_opcoes():
         print('1. Cadastrar restaurantes.')
         print('2. Listar restaurantes.')
         print('3. Ativar restaurantes.')
         print('4. Sair\n')
 
+#finalizar o app
 def finalizar_app():
-        os.system('cls')
-        print('Finalizando o app.')
+        exibir_subtitulo('Finalizando o app.')
 
+#voltando para o menu principal
 def voltar_ao_menu():
         input('Digite uma tecla para voltar ao menu principal')
         main()
 
+#quando a opção que o usuário ubmeteu for invalida
 def opcao_invalida():
         print('Opcao invalida"\n')
         voltar_ao_menu()
 
-def cadastrar_novo_restaurante():
+#refatorando a ação limpar tela + nome da ação
+def exibir_subtitulo(texto):
         os.system('cls')
-        print('Cadastro de novos restaurantes.\n')
+        print(texto)
+        print()
+
+#cadastrar novo restaurante de array
+def cadastrar_novo_restaurante():
+        exibir_subtitulo('Cadastro de novos restaurantes.')
         nome_do_restaurate = input('Digite o nome do restaurante que deseja cadastar: ')
         restaurantes.append(nome_do_restaurate)
         print(f'O restaurate {nome_do_restaurate} foi cadastrado com sucesso.\n')
+
         voltar_ao_menu()
 
+#mostrar a lista de restaurantes do array
 def listar_restaurantes():
-        os.system('cls')
-        print('Listar os restaurantes.\n')
+        exibir_subtitulo('Listar os restaurantes.')
+        
         for restaurante in restaurantes:
                 print(f'.{restaurante}')
         voltar_ao_menu()
 
+#analisar melhor esta opção depois
+"""def excluir_restaurante():
+        for restaurante in restaurantes:
+                print(f'.{restaurante}')
+
+        nome_do_restaurante = input('\nQual restaurante deseja excluir?')
+        restaurantes.remove(nome_do_restaurante)
+        print('Restaurantes excluido')"""
+
+#quando a opção desejada é selecionada
 def escolher_opcao_1():
         try:
                 opcao_escolhida = int(input('Escolha uma opção: '))
@@ -54,11 +77,14 @@ def escolher_opcao_1():
                         opcao_invalida()
         except:
                 opcao_invalida()
+
+#sequencia das funções a serem assionadas
 def main():
         os.system('cls')
         exibir_nome_programa()
         exibir_opcoes()
         escolher_opcao_1()
 
+#main
 if __name__ == '__main__':
         main()
