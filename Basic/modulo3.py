f8846be96a3f73143bea3a3b2786222e4458ac42
estelah <emfj.estela@gmail.com>
2024-06-21 05:39:44 -0300

    Add new .py

---
+++ Basic/modulo3.py
@@ -0,0 +1,22 @@
+#Exercicios
+
+#1. listas
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numerosPrimos = [3, 5, 7, 11]
+ano = [1998, 2024]
+
+#2. percorrendo lista com loop
+def percorrerLista():
+    for numero in numeros:
+        print(numero)
+
+#3. calculando números impares
+
+
+def main():
+    percorrerLista()
+
+
+
+if __name__ == "__main__":
+    main()
